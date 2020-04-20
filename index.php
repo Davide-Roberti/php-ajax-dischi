6327a46b490dbb77a735864890ecb96a26c7b513
Davide <davide.roberti89@gmail.com>
2020-04-20 13:55:06 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="dist/css/style.css">
+        <title>dischi</title>
+        <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+        <script src="dist/js/main.js" charset="utf-8"></script>
+    </body>
+</html>
